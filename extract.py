089e053d03036e5895a56d0b1c0f7a18272cfedb
Alan Sabino <alan.sabino@usp.br>
2020-02-19 17:12:36 -0300

    Adiciona nova forma de stitch

--- extract.py
+++ extract.py
@@ -30,18 +30,20 @@
 
 # USAGE
 # python extract.py --path midia/016-2017
+# python extract.py --path midia/tres
 
 import cv2 as cv
 import numpy as np
 import argparse
 import os
+import sys
 
 def video_to_frame(source):
   vidcap = cv.VideoCapture(source)
   success,image = vidcap.read()
   count = 0
   path = os.path.splitext(source)[0]
-  os.mkpath(path)
+  os.mkdir(path)
   while success:
     gray_frame = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     cv.imwrite(path+"/frame%03d.png" % count, gray_frame)
@@ -58,7 +60,7 @@ def video_to_frame_cropped(source):
   os.mkdir(path)
   while success:
     gray_frame = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    crop_img = gray_frame[83:498, 83:498]
+    crop_img = gray_frame[85:495, 85:495]
     cv.imwrite(path+"/frame%03d.png" % count, crop_img)
     success,image = vidcap.read()
     count += 1
@@ -82,6 +84,65 @@ def stich_stack(source):
   cv.imwrite("teste.png", pano)
   print("stitching completed successfully.")
 
+def stich_loop(source):
+  list_images = sorted(os.listdir(source))
+  images = []
+  for image_name in list_images:
+    image = cv.imread(source+'/'+image_name)
+    images.append(image)
+
+  two_images = []
+  two_images.append(images.pop(0))
+  for image in range(0, len(images)):
+    two_images.append(images.pop(0))
+    result = stitch(two_images)
+
+    if result is None:
+      print("Can't stitch images", image)
+      sys.exit(-1)
+
+    two_images = []
+    two_images.append(result)
+
+  cv.imwrite("teste.png", two_images.pop(0))
+  print("stitching completed successfully.")
+  print(image)
+
+def stitch( images, ratio=0.75, reproj_thresh=4.0):
+  (imageB, imageA) = images
+  (kpsA, featuresA) = detectAndDescribe(imageA)
+  (kpsB, featuresB) = detectAndDescribe(imageB)
+  M = matchKeypoints(kpsA, kpsB, featuresA, featuresB, ratio, reprojThresh)
+  if M is None:
+    return None
+
+  (matches, H, status) = M
+  result = cv.warpPerspective(imageA, H, (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
+  result[0:imageB.shape[0], 0:imageB.shape[1]] = imageB
+
+  return result
+
+def detectAndDescribe(image):
+  descriptor = cv.xfeatures2d.SIFT_create(0,5,0.04,10,1.6)
+  (kps, features) = descriptor.detectAndCompute(image, None)
+  kps = np.float32([kp.pt for kp in kps])
+  return (kps, features)
+
+def matchKeypoints(kpsA, kpsB, featuresA, featuresB, ratio, reprojThresh):
+  matcher = cv.DescriptorMatcher_create("BruteForce")
+  rawMatches = matcher.knnMatch(featuresA, featuresB, 2)
+  matches = []
+  for m in rawMatches:
+    if len(m) == 2 and m[0].distance < m[1].distance * ratio:
+      matches.append((m[0].trainIdx, m[0].queryIdx))
+
+  if len(matches) > 4:
+    ptsA = np.float32([kpsA[i] for (_, i) in matches])
+    ptsB = np.float32([kpsB[i] for (i, _) in matches])
+    (H, status) = cv.findHomography(ptsA, ptsB, cv.RANSAC, reprojThresh)
+    return (matches, H, status)
+  return None
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--path", type=str, required=True,
 	help="path to input directory of images")
@@ -91,14 +152,5 @@ source = args["path"]
 
 #video_to_frame(source)
 #video_to_frame_cropped(source)
-stich_stack(source)
-
-# Rascunho recortar mascara de borda
-#  #_,thresh = cv2.threshold(gray,1,255,cv2.THRESH_BINARY)
-#  #contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-#  #cnt = contours[0]
-#  #x,y,w,h = cv2.boundingRect(cnt)
-#  #crop = image[y:y+h,x:x+w]
-#  #crop = thresh
-#  #cv2.imwrite("fps/frame%d.png" % count, crop)     # save frame as JPEG file
-#  cv.imwrite("fps/frame%d.png" % count, gray)     # save frame as JPEG file
+#stich_stack(source)
+stich_loop(source)
